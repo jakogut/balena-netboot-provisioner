3ad4e2eb83db240ea8e87bb06347d238fc97b3ae
Joseph Kogut <joseph@balena.io>
2022-05-04 10:52:29 -0700

    asset-manager: parameterize initramfs inputs
    Make initramfs script functions accept parameters, so they're callable
    externally
    
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- src/asset-manager/initramfs.sh
+++ src/asset-manager/initramfs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-find_root() {
+find_hostapp() {
 	BINDIR="$(find "$1" -name "busybox")"
 	dirname "$(dirname "${BINDIR}")"
 }
 
-ROOTDIR="$(find_root "$1")"
-
 find_binary() {
-	find "${ROOTDIR}" -name "$1"
+	local rootdir=$2
+	find "${rootdir}" -name "$1"
 }
 
 find_deps() {
@@ -16,19 +15,19 @@ find_deps() {
 		| awk '{ print $2 }'
 }
 
-WANTED_BINARIES="dd curl"
-
 is_absolute() {
 	[[ "$1" == /* ]]
 }
 
 install_binary() {
+	local rootdir="${2}"
+	local outdir="${3}"
 	local path
 	local deps
 	local src
 	local dest
 
-	path="$(find_binary "$1")"
+	path="$(find_binary "$1" "${rootdir}")"
 	if [ -z "${path}" ]; then
 		echo "Unable to find binary: '${b}'"
 		exit 1;
@@ -36,26 +35,44 @@ install_binary() {
 
 	deps="$(find_deps "${path}")"
 	for d in ${deps}; do
-		install_binary "${d}"
+		install_binary "${d}" "${rootdir}" "${outdir}"
 	done
 
 	src="$(
 		if [ -L "${path}" ]; then
 			link="$(readlink "${path}")"
 			if is_absolute "${link}"; then
-				echo "${ROOTDIR}${link}"
+				echo "${rootdir}${link}"
 			else
 				readlink -f "${path}"
 			fi
 		else
 			echo "${path}";
 		fi)"
-	dest="$(if [[ "${path}" == *.so* ]]; then echo initramfs/lib; else echo initramfs/bin; fi)"
+	dest="$(
+		if [[ "${path}" == *.so* ]]; then
+			echo "${outdir}/lib";
+		else
+			echo "${outdir}/bin";
+		fi
+	)"
+
+	mkdir -p "${dest}"
 	cp -v "${src}" "${dest}/${1}"
 }
 
-mkdir -p initramfs/{bin,lib}
+populate_initramfs() {
+	local wanted_binaries=${1}
+	local outdir="${2}"
+	local hostapp_root="${3}"
 
-for b in ${WANTED_BINARIES}; do
-	install_binary "$b"
-done
+	for b in ${wanted_binaries}; do
+		install_binary "$b" "${hostapp_root}" "${outdir}"
+	done
+}
+
+generate_initramfs() {
+	local srcdir="${1}"
+	local output="${2}"
+	(cd "${srcdir}" || exit; find . | cpio -o -H newc | gzip) > "${output}"
+}
