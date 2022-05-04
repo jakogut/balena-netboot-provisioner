b6fea3520131f491697c2dd9228a39f8b9f35f5f
Joseph Kogut <joseph@balena.io>
2022-05-04 10:51:04 -0700

    asset-manager: cache os image
    Skip downloading the OS image when it's already in place and the same
    size as the remote resource.
    
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- src/asset-manager/entry.sh
+++ src/asset-manager/entry.sh
@@ -20,21 +20,45 @@ while ! [ -f /certs/.ready ]; do sleep "$(( (RANDOM % 5) + 5 ))s"; done
 # export CURL_CA_BUNDLE) or https://serverfault.com/questions/485597/default-ca-cert-bundle-location
 cat < /certs/ca-bundle.pem | openssl x509 -noout -text
 
-# $1: fleet ID
-# $2: API key
 download_balenaos() {
-    local fleet_id=${1}
-    local api_key=${2}
-    local version=${3}
-    local filetype=${4:-.gz}
-    local output_dir=${5:-/data/${fleet_id}/}
-    curl "${BALENA_API_URL}/download" \
-        --get \
-        --header "Authorization: Bearer ${api_key}" \
-        --data "version=${version}" \
-        --data "fileType=${filetype}" \
-        --data "appId=${fleet_id}" \
-        --output "${output_dir}/balenaos.img"
+	local fleet_id=${1}
+	local api_key=${2}
+	local version=${3}
+	local output=$4
+	local filetype=".gz"
+	local local_filsz
+	local remote_filsz
+	local request_args=(
+		--get
+		--header "Authorization: Bearer ${api_key}"
+		--data "version=${version}"
+		--data "fileType=${filetype}"
+		--data "appId=${fleet_id}"
+	)
+	local_filsz=$(du -b "${output}.gz" | cut -f1 || echo 0)
+	remote_filsz=$(curl "${BALENA_API_URL}/download" \
+			--head \
+			"${request_args[@]}" \
+		| grep -i content-length \
+		| cut -d' ' -f2 \
+		| tr -d '\r'
+	)
+
+	# Skip the download if the existing filesize matches the remote.
+	# Unfortunately, the reported content-length from the image maker
+	# is non-deterministic, often changing by several bytes every request.
+	# Compute a delta and ensure it's below the threshold to work around this.
+	local filsz_delta=$(( remote_filsz - local_filsz ))
+	local filsz_delta_thresh=10
+	if [ "${filsz_delta#-}" -gt "${filsz_delta_thresh}" ]; then
+		curl "${BALENA_API_URL}/download" \
+			"${request_args[@]}" \
+			--output "${output}.gz"
+	else
+		echo "OS image already exists and size matches remote, skipping download"
+	fi
+
+	[ -f "${output}" ] || gunzip -c "${output}.gz" > "${output}"
 }
 
 if [ -z "${FLEET_CONFIG}" ] || [[ ! "${FLEET_CONFIG}" = *:* ]]; then
